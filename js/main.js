c349f3ed3e09a4677e08888db0f4e0f0d46e6d94
Dustin-Davis <drpookydavis@comcast.net>
2022-10-17 19:09:01 -0700

    wrote question

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ var checkIfPangram = function (sentence) {
   let regex = /([a-z])(?!.*\1)/gi;
   return (sentence.match(regex)).length === 26;
 };
+
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+// You can return the answer in any order.
